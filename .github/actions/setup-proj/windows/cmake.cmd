0e8026e9d6a3219f2ff473847fe7d6ea6728d496
Balázs Dukai <balazs.dukai@gmail.com>
2026-05-13 20:12:29 +0200

    Fix Windows PROJ MSYS2 build setup

--- .github/actions/setup-proj/windows/cmake.cmd
+++ .github/actions/setup-proj/windows/cmake.cmd
@@ -1,9 +1,12 @@
 @echo off
 setlocal
-set "PATH=C:\msys64\mingw64\bin;C:\mingw64\bin;C:\msys64\usr\bin;%PATH%"
-set "REAL_CMAKE=C:\msys64\mingw64\bin\cmake.exe"
-if not exist "%REAL_CMAKE%" set "REAL_CMAKE=C:\mingw64\bin\cmake.exe"
-if not exist "%REAL_CMAKE%" set "REAL_CMAKE=C:\Program Files\CMake\bin\cmake.exe"
+set "PATH=__MINGW_BIN__;__USR_BIN__;%PATH%"
+set "REAL_CMAKE=__CMAKE_EXE__"
+
+if not exist "%REAL_CMAKE%" (
+  echo Missing MSYS2 CMake at "%REAL_CMAKE%" 1>&2
+  exit /b 1
+)
 
 if "%~1"=="--build" goto passthrough
 if "%~1"=="--install" goto passthrough
@@ -11,9 +14,10 @@ if "%~1"=="--version" goto passthrough
 if "%~1"=="-E" goto passthrough
 
 call "%REAL_CMAKE%" ^
-  -DEXE_SQLITE3=__SQLITE_CMD__ ^
-  -DTIFF_INCLUDE_DIR=C:/msys64/mingw64/include ^
-  -DTIFF_LIBRARY=C:/msys64/mingw64/lib/libtiff.dll.a ^
+  -DEXE_SQLITE3=__SQLITE_EXE__ ^
+  -DTIFF_INCLUDE_DIR=__TIFF_INCLUDE_DIR__ ^
+  -DTIFF_LIBRARY=__TIFF_LIBRARY__ ^
+  -DCMAKE_MAKE_PROGRAM=__NINJA_EXE__ ^
   %*
 exit /b %ERRORLEVEL%
 
--- .github/actions/setup-proj/windows/sqlite3.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "PATH=C:\msys64\mingw64\bin;C:\mingw64\bin;C:\msys64\usr\bin;%PATH%"
-set "REAL_SQLITE=C:\msys64\mingw64\bin\sqlite3.exe"
-if not exist "%REAL_SQLITE%" set "REAL_SQLITE=C:\mingw64\bin\sqlite3.exe"
-if not exist "%REAL_SQLITE%" set "REAL_SQLITE=C:\msys64\usr\bin\sqlite3.exe"
-if not exist "%REAL_SQLITE%" set "REAL_SQLITE=sqlite3.exe"
-call "%REAL_SQLITE%" %*
-exit /b %ERRORLEVEL%
